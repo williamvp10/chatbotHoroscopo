e75782752d5fd43dbda2c2bfd69b38bc44d891d7
willi <willi@192.168.56.1>
2019-04-27 22:31:54 -0500

    IOT Chatbot otra prueba arreglado

--- src/main/java/chatbot/Chatbot.java
+++ src/main/java/chatbot/Chatbot.java
@@ -352,7 +352,7 @@ public class Chatbot {
         System.out.println("obj:" + obj);
         System.out.println(" " + obj.get("id").getAsString());
         e.add("titulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString() + " temperatura: " + obj.get("temperatura").getAsString() + "  humedad: " + obj.get("humedad").getAsString() + " presion: " + obj.get("presion").getAsString()));
-        e.add("subtitulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString() + " temperatura: " + obj.get("temperatura").getAsString() + "  humedad: " + obj.get("humedad").getAsString() + " presion: " + obj.get("presion").getAsString()));
+        e.add("subtitulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString()));
         e.add("url", new JsonPrimitive("" + "https://www.pce-instruments.com/espanol/slot/4/artimg/large/pce-instruments-sensor-de-temperatura-pce-ir-57-5638928_957363.jpg"));
         e.add("buttons", new JsonArray());
         elements.add(e);
@@ -423,7 +423,7 @@ public class Chatbot {
         e = new JsonObject();
         JsonObject obj = servicio.get("sensor").getAsJsonObject();
         e.add("titulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString() + " temperatura: " + obj.get("temperatura").getAsString() + "  humedad: " + obj.get("humedad").getAsString() + " presion: " + obj.get("presion").getAsString()));
-        e.add("subtitulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString() + " temperatura: " + obj.get("temperatura").getAsString() + "  humedad: " + obj.get("humedad").getAsString() + " presion: " + obj.get("presion").getAsString()));
+        e.add("subtitulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString()));
         e.add("url", new JsonPrimitive("" + "https://www.pce-instruments.com/espanol/slot/4/artimg/large/pce-instruments-sensor-de-temperatura-pce-ir-57-5638928_957363.jpg"));
         e.add("buttons", new JsonArray());
         elements.add(e);
@@ -542,7 +542,7 @@ public class Chatbot {
             System.out.println("obj:" + obj);
             e.add("titulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString()));
             e.add("subtitulo", new JsonPrimitive("id: " + obj.get("id").getAsString()));
-            e.add("url", new JsonPrimitive(obj.get("url").getAsString()));
+            e.add("url", new JsonPrimitive("" + "https://www.pce-instruments.com/espanol/slot/4/artimg/large/pce-instruments-sensor-de-temperatura-pce-ir-57-5638928_957363.jpg"));
             b = new JsonObject();
             b1 = new JsonArray();
             b.add("titulo", new JsonPrimitive("Seleccionar"));
