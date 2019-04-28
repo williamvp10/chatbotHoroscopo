1cb69baf02205206325bc3bb6d346317b2679c8f
willi <willi@192.168.56.1>
2019-04-27 23:44:23 -0500

    IOT Chatbot otra prueba arreglado

--- src/main/java/chatbot/Chatbot.java
+++ src/main/java/chatbot/Chatbot.java
@@ -496,18 +496,15 @@ public class Chatbot {
         JsonArray buttons = new JsonArray();
         JsonObject b = null;
         JsonArray b1 = null;
-        JsonObject obj = null;
         JsonArray elements = new JsonArray();
         JsonObject e = null;
-        JsonObject servicio = service.getModificarSensor(this.sensor);
-        JsonArray elementosServicio = (JsonArray) servicio.get("sensor").getAsJsonArray();
-
-        for (int i = 0; i < elementosServicio.size(); i++) {
-            e = new JsonObject();
-            obj = elementosServicio.get(i).getAsJsonObject();
-            e.add("titulo", new JsonPrimitive("" + "se a modificado el sensor"));
-            elements.add(e);
-        }
+        JsonObject obj = service.getModificarSensor(this.sensor);
+        e = new JsonObject();
+        e.add("titulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString() + " temperatura: " + obj.get("temperatura").getAsString() + "  humedad: " + obj.get("humedad").getAsString() + " presion: " + obj.get("presion").getAsString()));
+        e.add("subtitulo", new JsonPrimitive("" + "id: " + obj.get("id").getAsString()));
+        e.add("url", new JsonPrimitive("" + "https://www.pce-instruments.com/espanol/slot/4/artimg/large/pce-instruments-sensor-de-temperatura-pce-ir-57-5638928_957363.jpg"));
+        e.add("buttons", new JsonArray());
+        elements.add(e);
         out.add("buttons", buttons);
         out.add("elements", elements);
         return out;
